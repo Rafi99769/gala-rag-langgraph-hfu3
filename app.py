80997e93e6b88d5b8acd6b90538fa6f4607487c5
Rafi99769 <ahnafhossainrafi@gmail.com>
2025-04-12 02:22:23 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+from typing import TypedDict, Annotated
+from dotenv import load_dotenv
+from langgraph.graph.message import add_messages
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.messages import AnyMessage, HumanMessage, AIMessage
+from langchain_openai import AzureChatOpenAI
+from retriever import guest_info_tool
+
+load_dotenv()
+
+tools = [guest_info_tool]
+
+llm = AzureChatOpenAI(
+    azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
+    azure_deployment="gpt-4o",
+    api_version="2025-01-01-preview",
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+llm_with_tools = llm.bind_tools(tools)
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+def assistant(state: AgentState):
+    return {
+        "messages": [llm_with_tools.invoke(state["messages"])]
+    }
+
+builder = StateGraph(AgentState)
+
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges("assistant", tools_condition)
+builder.add_edge("tools", "assistant")
+
+alfred = builder.compile()
+
+messages = [HumanMessage(content="Alfred, who is that gentleman talking to the ambassador?")]
+response = alfred.invoke({"messages": messages})
+
+print("🎩 Alfred's Response:")
+print(response['messages'][-1].content)
